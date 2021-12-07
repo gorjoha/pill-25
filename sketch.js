7dba85e51706b8d41310d345473e53989d79d4e3
gorjoha <93801364+gorjoha@users.noreply.github.com>
2021-12-07 16:02:17 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,38 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+var dustbinObj,groundObject	
+var world;
+var papelbola;
+
+function setup() {
+	createCanvas(1600, 700);
+	rectMode(CENTER);
+
+
+	engine = Engine.create();
+	world = engine.world;
+	
+	groundObject=new ground(width/2,670,width,20);
+	dustbinObj=new dustbin(1200,650);
+	papelbola=new Papel(140,10,70,70);
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(230);
+ 
+
+  groundObject.display();
+  dustbinObj.display();
+  papelbola.display();
+  if(keyDown(UP_ARROW)){
+	Matter.Body.applyForce(papelbola.body,papelbola.body.position,{x:130,y:-145});
+  }
+}
